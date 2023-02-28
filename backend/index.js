5148c93f00542ab646a296ea78db850eb808e015
Vivek Modi <vivek.modi@dotsquares.com-at-134080855983>
2023-02-28 13:37:11 +0530

    install basic packages

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
+const dotenv = require('dotenv').config().parsed;
 const app = express()
-const port = 3001
-
+const port = dotenv.PORT
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
